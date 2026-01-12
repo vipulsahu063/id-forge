f650387701f34dddaa6e07615b181e2871f412ef
Vipul Sahu <vipulsahu063@gmail.com>
2026-01-12 19:35:34 +0530

    fixed middleware error

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,10 @@
-export { default } from "next-auth/middleware";
+import { default as middleware } from "next-auth/middleware";
+
+export { middleware };  // Named export that Next.js 16 recognizes
 
 export const config = {
   matcher: [
     "/admin/:path*",
     "/station/add-officer/:path*",
   ]
-}
+};
